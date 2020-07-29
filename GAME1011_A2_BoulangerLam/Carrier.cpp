02372262543260812159713ae8a1335f926351f7
boulanger14 <43892813+boulanger14@users.noreply.github.com>
2020-07-29 15:42:09 -0400

    carrier class

--- GAME1011_A2_BoulangerLam/Carrier.cpp
+++ GAME1011_A2_BoulangerLam/Carrier.cpp
@@ -1,1 +1,61 @@
 #include "Carrier.h"
+
+Carrier::Carrier(string name, int maxBays)
+{
+	this->name = name;
+	this->maxBays = maxBays;
+	numFighters = 0;
+	bayList[maxBays];
+}
+
+void Carrier::loadFighter(Fighter* nFighter)
+{
+	if (maxBays > numFighters)
+	{
+		bayList[numFighters] = nFighter;
+		numFighters += 1;
+	}
+}
+
+Fighter* Carrier::launchNextFighter()
+{
+	if (hasFighters())
+	{
+		numFighters--;
+		Fighter* nextFighter = bayList[numFighters];
+		bayList[numFighters] = nullptr;
+		return nextFighter;
+	}
+	else
+		return nullptr;
+}
+
+string Carrier::getInfo()
+{
+	string info = name += "\n";
+	for (int i = 0; i < numFighters; i++)
+	{
+	//	name += bayList[i].getInfo() + "\n";
+	}
+	return info;
+}
+
+bool Carrier::hasFighters()
+{
+	return (numFighters > 0);
+}
+
+int Carrier::getCapacity()
+{
+	return maxBays;
+}
+
+int Carrier::getNumFighters()
+{
+	return numFighters;
+}
+
+string Carrier::getName()
+{
+	return name;
+}
--- GAME1011_A2_BoulangerLam/Carrier.h
+++ GAME1011_A2_BoulangerLam/Carrier.h
@@ -1,8 +1,27 @@
 #pragma once
+
+#include <string>
+#include "Fighter.h"
+using namespace std;
+
 class Carrier
 {
-public:
-
 private:
+	string name;
+	int maxBays, numFighters;
+	Fighter** bayList;
+
+public:
+	Carrier() = default;
+	Carrier(string name, int maxBays);
+
+	void loadFighter(Fighter* nFighter);
+	Fighter* launchNextFighter();
+
+	string getInfo();
+	bool hasFighters();
+	int getCapacity();
+	int getNumFighters();
+	string getName();
 };
 
