a11afedd92d8960cf92888cca4b13a49e7ff3ecc
ab-gata <pauleen.lam@outlook.com>
2020-07-30 12:49:20 -0400

    Fighter class

---
+++ GAME1011_A2_BoulangerLam/Fighter.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+using namespace std;
+class Fighter
+{
+private:
+	string fName;
+	int damage;
+	int structStrength;
+public:
+	Fighter(string name, int damage, int str);
+	void reduceStructure(int damage);
+	string getFName();
+	int getDamage();
+	int getStructStrength();
+	void getInfo();
+};
+
